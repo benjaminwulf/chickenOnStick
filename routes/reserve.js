6ebc1aff412302e7a53510132780fb99307ef7eb
Benjamin W Wulf <benjaminwulf@gmail.com>
2018-08-14 20:44:21 -0600

    trying to get mustache and jade files to work

--- routes/reserve.js
+++ routes/reserve.js
@@ -1,3 +1,12 @@
-app.get("/reserve", function(req, res){
-    res.sendFile(path.join(__dirname), "reserve.html")
+var express = require('express');
+var router = express.Router();
+
+router.get("/reserve", function(req, res){
+    res.render('reserve', {reserve})
 })
+
+module.exports = router;
+
+// app.get("/reserve", function(req, res){
+//     res.sendFile(path.join(__dirname), "reserve.html")
+// })
